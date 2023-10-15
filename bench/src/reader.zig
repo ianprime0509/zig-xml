11f9db5b3d1a6085bfaad6946bcad8fd7512fbef
Ian Johnson <ian@ianjohnson.dev>
2023-10-14 22:42:27 -0400

    feat: move decoder type to options struct (#23)
    Closes #20
    
    This makes the API simpler to use. No flexibility is sacrificed, because
    users who want to pass in a non-default-initialized decoder object can
    just use the `TokenReader` and `Reader` `init` functions directly.

--- bench/src/reader.zig
+++ bench/src/reader.zig
@@ -5,7 +5,9 @@ pub const main = @import("common.zig").main;
 
 pub fn runBench(data: []const u8) !void {
     var data_stream = std.io.fixedBufferStream(data);
-    var reader = xml.reader(std.heap.c_allocator, data_stream.reader(), xml.encoding.Utf8Decoder{}, .{});
+    var reader = xml.reader(std.heap.c_allocator, data_stream.reader(), .{
+        .DecoderType = xml.encoding.Utf8Decoder,
+    });
     defer reader.deinit();
     while (try reader.next()) |_| {}
 }
--- bench/src/token_reader.zig
+++ bench/src/token_reader.zig
@@ -5,6 +5,8 @@ pub const main = @import("common.zig").main;
 
 pub fn runBench(data: []const u8) !void {
     var data_stream = std.io.fixedBufferStream(data);
-    var token_reader = xml.tokenReader(data_stream.reader(), xml.encoding.Utf8Decoder{}, .{});
+    var token_reader = xml.tokenReader(data_stream.reader(), .{
+        .DecoderType = xml.encoding.Utf8Decoder,
+    });
     while (try token_reader.next()) |_| {}
 }
